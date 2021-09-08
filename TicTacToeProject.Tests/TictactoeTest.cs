7ed5e2a5cde922794a7d05a6d35db86d0bcce421
dlanaras <dimitrislanaras04@outlook.com>
2021-09-08 14:25:07 +0200

    tictactoe tests added

---
+++ TicTacToeProject.Tests/TictactoeTest.cs
@@ -0,0 +1,46 @@
+using System;
+using Bunit;
+using Bunit.TestDoubles;
+using Xunit;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace TicTacToeProject.Pages
+{
+    public class TictactoeTest : TestContext
+    {
+
+        [Fact]
+        public void PlayerChoosingCrossLeadsToStartGame()
+        {
+
+            // Arrange
+            using var ctx = new TestContext();
+            ctx.Services.AddSingleton<GameStateService>();
+            ctx.Services.AddSingleton<ScoreTrackerService>();
+            var cut = ctx.RenderComponent<Tictactoe>();
+
+            // Assert that content of the paragraph shows counter at zero
+            cut.Find("button.cross").Click();
+
+            cut.Find(".startButton").MarkupMatches("<div class='startButton'><button class='btn btn-primary'>Start Game </button></div>");
+        }
+
+
+        [Fact]
+        public void PlayerChoosingCircleLeadsToStartGame()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+            ctx.Services.AddSingleton<GameStateService>();
+            ctx.Services.AddSingleton<ScoreTrackerService>();
+            var cut = ctx.RenderComponent<Tictactoe>();
+
+            // Act - click button to increment counter
+            cut.Find("button.circle").Click();
+
+            cut.Find(".startButton").MarkupMatches("<div class='startButton'><button class='btn btn-primary'>Start Game </button></div>");
+        }
+    }
+}
